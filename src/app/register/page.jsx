1cb3b0f0a19ef84687d6d974d917d82baefbf4dd
herlinpriatna <herlinpriatna@student.telkomuniversity.ac.id>
2025-06-14 15:12:31 +0700

    y

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -25,7 +25,7 @@ export default function AuthPage() {
   setMessage('')
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/register`, {
+    const res = await fetch('/api/v1/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
