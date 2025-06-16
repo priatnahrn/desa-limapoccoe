598800c6f87b7f0f3519fb5693209d51967eb0e6
herlinpriatna <herlinpriatna@student.telkomuniversity.ac.id>
2025-06-16 19:57:29 +0700

    y

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://quiet-pans-appear.loca.lt/api/:path*' // Proxy to external API
-            },
-        ];
-    }
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/:path*',
+    //             destination: 'https://quiet-pans-appear.loca.lt/api/:path*' // Proxy to external API
+    //         },
+    //     ];
+    // }
 };
 
 export default nextConfig;
